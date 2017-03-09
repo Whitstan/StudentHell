af5bf468b3c17f27a1f4ee30efbcac0aa56f0b14
Horváth Attila <attilazero@gmail.com>
2017-03-09 18:51:51 +0100

    firstNB
    firstNB

---
+++ src/studenthell/StudentHell.java
@@ -0,0 +1,10 @@
+package studenthell;
+
+public class StudentHell{
+    
+    public static void main(String[] args) {
+        Game game = new Game("StudentHell", 800, 600);
+        game.start();
+    }
+    
+}
